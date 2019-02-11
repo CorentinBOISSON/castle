4dcdb78ef3490c21c053871c38584ca4411d93b5
Corentin BOISSON <corentin.boisson@devinci.fr>
2019-02-11 17:36:53 +0100

    fichiers

--- .idea/public/relaisetchateau.js
+++ .idea/public/relaisetchateau.js
@@ -12,3 +12,25 @@ function createPromise() {
     let url = 'https://www.relaischateaux.com/fr/site-map/etablissements'
     promiseList.push(fillHotelsList(/*proxyUrl + */url));
 }
+
+function createIndividualPromises() {
+    return new Promise(function (resolve, reject) {
+        if (scraping === 0) {
+            for (i = 0; i < Math.trunc(ListeRelaisetChateau.length / 2); i++) {
+                let hotelURL = ListeRelaisetChateau [i].url;
+                indivPromisesList.push(infos(/*proxyUrl + */hotelURL, i));
+
+            }
+            resolve();
+            scraping++;
+        }
+        else if (scraping === 1) {
+            for (i = ListeRelaisetChateau.length / 2; i < Math.trunc(ListeRelaisetChateau.length); i++) {
+                let hotelURL = ListeRelaisetChateau [i].url;
+                indivPromisesList.push(infos(/*proxyUrl + */hotelURL, i));
+
+            }
+            resolve();
+        }
+    })
+}
