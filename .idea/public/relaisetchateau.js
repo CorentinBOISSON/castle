5c4119acf2f1f498d0cd47d90bd4cc04945a59a8
Corentin BOISSON <corentin.boisson@devinci.fr>
2019-02-11 17:42:51 +0100

    fichiers

--- .idea/public/relaisetchateau.js
+++ .idea/public/relaisetchateau.js
@@ -38,7 +38,7 @@ function ListesRC(url) {
                 chefname = chefname.replace(/\n/g, "");
                 hotelsList.push({ "name": name.trim(), "postalCode": "", "chef": chefname.trim(), "url": url, "price": "" })
             })
-            resolve(hotelsList);
+            resolve(ListeRelaisetChateau);
         });
     });
 }
@@ -63,5 +63,38 @@ function createIndividualPromises() {
             resolve();
         }
     })
+}
+
+
+function infos(url, index) {
+    return new Promise(function (resolve, reject) {
+        request(url, function (err, res, html) {
+            if (err) {
+                console.error(err);
+                return reject(err);
+            }
+            else if (res.statusCode !== 200) {
+                err = new Error("Unexpected status code : " + res.statusCode);
+                err.res = res;
+                return reject(err);
+            }
+
+            const $ = cheerio.load(html);
+
+            $('span[itemprop="postalCode"]').first().each(function () {
+                let data = $(this);
+                let pc = data.text();
+                ListeRelaisetChateau[index].postalCode = String(pc.split(',')[0]).trim();
+            })
+
+            $('.price').first().each(function () {
+                let data = $(this);
+                let price = data.text();
+                ListeRelaisetChateau[index].price = String(price);
+            })
+
+            resolve(ListeRelaisetChateau);
+        });
+    });
 }
 
