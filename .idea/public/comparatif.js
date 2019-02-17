20cb7e1e063d5d8572e4afc551e539544bba2986
Corentin BOISSON <corentin.boisson@devinci.fr>
2019-02-17 23:16:10 +0100

    fichiers

--- .idea/public/comparatif.js
+++ .idea/public/comparatif.js
@@ -2,6 +2,7 @@ const Promise = require('promise');
 const relaisetchateau = require('./relaisetchateau.js');
 const michelin = require('./michelin.js');
 var fs = require('fs');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 
 'use strict';
@@ -23,3 +24,27 @@ function findMutualChefsAndPCs(hotelsList, michelinsList) {
     return starredHotels;
 }
 
+function readJSON(path) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function(e) {
+        if (this.status === 200) {
+            let file = new File([this.response], 'temp');
+            let fileReader = new FileReader();
+            fileReader.addEventListener('load', function(){
+                console.log(fileReader.result[0].price);
+                for (let i = 0; i < 10; i++){
+                    console.log("Hotel " + i + ": ");
+                    console.log(fileReader.result[i].hotelName);
+                    console.log("Prix:");
+                    console.log(fileReader.result[i].price);
+                }
+            });
+            fileReader.readAsText(file);
+        }
+    };
+    xhr.send();
+}
+
+readJSON("RelaisetChateauEtoiles.json");
